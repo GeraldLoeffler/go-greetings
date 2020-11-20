7f0232f1569863e07e982ddee3ecca835abb1fc4
GeraldLoeffler <gerald.loeffler@googlemail.com>
2020-11-20 14:30:33 +0100

    better tests

--- greetings/greet_test.go
+++ greetings/greet_test.go
@@ -5,19 +5,26 @@ import (
 	"testing"
 )
 
-func TestGreet(t *testing.T) {
-	n := "Gerald"
-	want := regexp.MustCompile("\\b" + n + "\\b")
-	g, err := Greet(n)
-	if err != nil || !want.MatchString(g) {
-		t.Fatalf("Greet(%v)=%q,%v want match to regex", n, g, err)
+func TestGreetHappy(t *testing.T) {
+	cases := []struct {
+		in   string
+		want *regexp.Regexp
+	}{
+		{"Gerald", regexp.MustCompile("\\bGerald\\b")},
+		{"Dagmar", regexp.MustCompile("\\bDagmar\\b")},
+	}
+	for _, c := range cases {
+		got, err := Greet(c.in)
+		if err != nil || !c.want.MatchString(got) {
+			t.Errorf("Greet(%q) = (%q, %q), want match to %q", c.in, got, err, c.want)
+		}
 	}
 }
 
-func TestGreetEmpty(t *testing.T) {
+func TestGreetError(t *testing.T) {
 	n := ""
-	g, err := Greet(n)
-	if err == nil || g != "" {
-		t.Fatalf("Greet(%v)=%q,%v want error", n, g, err)
+	got, err := Greet(n)
+	if err == nil || got != "" {
+		t.Fatalf("Greet(%q) = (%q, %q), want error", n, got, err)
 	}
 }
